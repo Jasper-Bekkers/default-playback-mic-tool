a3d4ecad9f8e88fd0542dcca97bf131ddcc293fc
Jasper-Bekkers <bekkers@gmail.com>
2018-11-22 14:09:42 +0100

    Change default playback & mic tool

---
+++ DefaultAudioDevice/Source.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <tchar.h>
+#include "windows.h"
+#include "Mmdeviceapi.h"
+#include "PolicyConfig.h"
+#include "Propidl.h"
+#include "Functiondiscoverykeys_devpkey.h"
+
+HRESULT SetDefaultAudioPlaybackDevice(LPCWSTR devID)
+{
+	IPolicyConfigVista *pPolicyConfig;
+	ERole reserved = eConsole;
+
+	HRESULT hr = CoCreateInstance(__uuidof(CPolicyConfigVistaClient),
+		NULL, CLSCTX_ALL, __uuidof(IPolicyConfigVista), (LPVOID *)&pPolicyConfig);
+	if (SUCCEEDED(hr))
+	{
+		hr = pPolicyConfig->SetDefaultEndpoint(devID, reserved);
+		pPolicyConfig->Release();
+	}
+	return hr;
+}
+
+void changeDefault(IMMDeviceEnumerator *pEnum, EDataFlow dataFlow, const wchar_t* defaultName)
+{
+	IMMDeviceCollection *pDevices;
+	// Enumerate the output devices.
+	HRESULT hr = pEnum->EnumAudioEndpoints(dataFlow, DEVICE_STATE_ACTIVE, &pDevices);
+
+	IMMDevice *pDefaultDevice = NULL;
+	hr = pEnum->GetDefaultAudioEndpoint(dataFlow, eConsole, &pDefaultDevice);
+
+	LPWSTR defaultDeviceWstrID = NULL;
+	hr = pDefaultDevice->GetId(&defaultDeviceWstrID);
+
+	UINT count;
+	pDevices->GetCount(&count);
+
+	bool changed = false;
+	for (int i = 0; i < count; i++)
+	{
+		IMMDevice *pDevice;
+		hr = pDevices->Item(i, &pDevice);
+
+		LPWSTR wstrID = NULL;
+		hr = pDevice->GetId(&wstrID);
+
+		IPropertyStore *pStore;
+		hr = pDevice->OpenPropertyStore(STGM_READ, &pStore);
+
+		PROPVARIANT friendlyName;
+		PropVariantInit(&friendlyName);
+		hr = pStore->GetValue(PKEY_Device_FriendlyName, &friendlyName);
+
+		// if no options, print the device
+		// otherwise, find the selected device and set it to be default
+		printf("Audio Device %d: %ws\n", i, friendlyName.pwszVal);
+
+		if (!!wcscmp(wstrID, defaultDeviceWstrID))
+		{
+			if (!wcscmp(friendlyName.pwszVal, defaultName))
+			{
+				if (!changed)
+				{
+					SetDefaultAudioPlaybackDevice(wstrID);
+					changed = true;
+				}
+			}
+		}
+
+		PropVariantClear(&friendlyName);
+		pStore->Release();
+		pDevice->Release();
+	}
+
+	pDevices->Release();
+	pDefaultDevice->Release();
+}
+
+// EndPointController.exe [NewDefaultDeviceID]
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// read the command line option, -1 indicates list devices.
+	int option = -1;
+	if (argc == 2) option = atoi((char*)argv[1]);
+
+	auto defaultPlaybackDeviceName = L"Speakers / Headphones (Realtek Audio)";
+	auto defaultMicrophoneDeviceName = L"Microphone (Samson C01U Pro Mic)";
+
+	HRESULT hr = CoInitialize(NULL);
+	if (SUCCEEDED(hr))
+	{
+		IMMDeviceEnumerator *pEnum = NULL;
+		// Create a multimedia device enumerator.
+		hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL,
+			CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnum);
+		if (SUCCEEDED(hr))
+		{
+			changeDefault(pEnum, eRender, defaultPlaybackDeviceName);
+			changeDefault(pEnum, eCapture, defaultMicrophoneDeviceName);
+
+			pEnum->Release();
+		}
+	}
+	return hr;
+}
